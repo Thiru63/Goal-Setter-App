87a81409465e500a4c9fc848126b43d521eb7766
Thiru63 <100009930+Thiru63@users.noreply.github.com>
2023-04-20 11:25:59 +0530

    push

--- backend/server.js
+++ backend/server.js
@@ -23,20 +23,20 @@ app.use('/api/users', require('./routes/userRoutes'));
 
 
 // Serve frontend
-// if (process.env.NODE_ENV === 'production') {
-//     app.use(express.static(path.join(__dirname, '../frontend/build')));
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/build')));
   
-//     app.get('*', (req, res) =>{
-//       res.sendFile(
-//         path.join(__dirname, "../frontend/build/index.html"),
-//         function (err){
-//             res.status(500).send(err);
-//         }
-//       );
-// });
-//   } else {
-//     app.get('/', (req, res) => res.send('Please set to production'));
-//   }
+    app.get('*', (req, res) =>{
+      res.sendFile(
+        path.join(__dirname, "../frontend/build/index.html"),
+        function (err){
+            res.status(500).send(err);
+        }
+      );
+});
+  } else {
+    app.get('/', (req, res) => res.send('Please set to production'));
+  }
 
 app.use(errorHandler);
 
